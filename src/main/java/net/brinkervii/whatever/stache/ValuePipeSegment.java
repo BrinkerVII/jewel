356916f14a3a4ef1dabf16b7cc947ec38ded88af
BrinkerVII <brinkervii@gmail.com>
2018-05-20 22:45:26 +0200

    Correct value replace behaviour

--- src/main/java/net/brinkervii/whatever/stache/ValuePipeSegment.java
+++ src/main/java/net/brinkervii/whatever/stache/ValuePipeSegment.java
@@ -5,6 +5,7 @@ import net.brinkervii.whatever.stache.piping.StachePipelineState;
 import org.jsoup.nodes.Element;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class ValuePipeSegment extends StachePipelineStage {
 	public ValuePipeSegment(StachePipelineState state, String bit) {
@@ -31,7 +32,7 @@ public class ValuePipeSegment extends StachePipelineStage {
 		}
 
 		if (result != null) {
-			work.text(result.toString());
+			work.text(work.ownText().replaceAll(String.format("\\{\\{\\s*%s\\s*}}", bit), result.toString()));
 		}
 
 		return work;
